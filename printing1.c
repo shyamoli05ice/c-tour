99b22fe01b1ec391b699bd849e01d28fee7dcb89
Shyamoli <shyamoli05ice@gmail.com>
2018-09-20 00:01:27 +0600

    completed 22b

---
+++ printing1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    /*for(i=1;i<=5;i++)
+    {
+        for(j=i;j<=5;j++)
+            printf("1");
+    }
+    printf("\n");*/
+
+    for(i=1;i<=5;i++) {
+       for(j = 0; j<=5-i; j++) {
+            printf("%d ",5-j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
